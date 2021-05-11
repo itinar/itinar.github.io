ade47796e1c793bbe8753d2fddb97870e5f3de4e
jochuannes <64692298+jochuannes@users.noreply.github.com>
2021-05-11 16:33:29 +0200

    asdf

---
+++ charte/SVG-World-Map/tables/pandas2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  7 22:46:13 2021
+
+@author: Johann
+"""
+import pandas as pd
+import os
+
+# I prefer reading excel with pd.read_excel
+# passing `sheet_name=None` returns a dictionary 
+# with the form {sheet_name: dataframe}
+#file = input("file name:")
+
+data = pd.read_excel('tableTest.xlsx', sheet_name=None)
+
+# loop through the dictionary and save csv
+for sheet_name, df in data.items():
+    os.makedirs(sheet_name)
+    df.to_csv(f'{sheet_name}'+'/'+f'{sheet_name}.csv')
